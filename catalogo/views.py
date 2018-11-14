b7700bce4888c59cb4646f4b0bec9027b8846c1c
liapino <l.ancavilpino@gmail.com>
2018-11-14 20:50:01 -0300

    Agregando views

--- catalogo/views.py
+++ catalogo/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def catalogo(request):
-	data = {}
-	template_name = 'catalogo.html'
-	return render(request, template_name, data)
+    template_name = "catalogo/catalogo.html"
+    #catalogo = Catalogo.objects.all()
+    
+    for i in Catalogo.objects.all():
+        print("---------")
+        print (i.bicicleta.marca)
+    
+    return render(request, template_name, {'catalogo':Catalogo.objects.all()})
